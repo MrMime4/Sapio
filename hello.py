6bac1066eb9b5190a6c63c7c281fcc65b8839d61
MrMime4 <manthanthadwe19499@gmail.com>
2021-02-25 22:03:42 +0530

    YOYO function created

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 print("Hello World")
+
+def yoyo():
+    print("YOYO")
 for i in range(9):
     if (i%2 == 0):
         print(i)
     else:
-        print(i,"is Odd number yoyo",end="")
-
+        print(i,"is Odd number",end="")
+yoyo()
