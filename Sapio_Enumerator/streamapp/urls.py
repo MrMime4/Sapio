f15606c1aec3a2ee5224708dfdac239495715b45
MrMime4 <manthanthadwe19499@gmail.com>
2021-04-24 18:03:35 +0530

    commit message

--- Sapio_Enumerator/streamapp/urls.py
+++ Sapio_Enumerator/streamapp/urls.py
@@ -4,10 +4,6 @@ from . import views
 app_name = 'streamapp'
 
 urlpatterns = [
-<<<<<<< HEAD
-=======
-    path('', views.home, name='home'),
->>>>>>> b5c60cd24ebed6da9eb4490924ee053df955720c
     path('stream/', views.stream, name='stream'),
     path('video_feed', views.video_feed, name='video_feed'),
     
